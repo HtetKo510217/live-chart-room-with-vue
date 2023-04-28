740b7d50db3558410706d930eaed188294608a66
HtetKo510217 <htetko510217@gmail.com>
2023-04-28 20:20:34 +0630

    Project setup

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <dvi class="welcome container">
+        <p>Welcome</p>
+    </dvi>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .welcome {
+        text-align: center;
+        padding: 20px 0;
+    }
+</style>
