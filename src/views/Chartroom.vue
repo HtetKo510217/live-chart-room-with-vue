171258c0e99ed1492ec316f797072045371e3f59
HtetKo510217 <htetko510217@gmail.com>
2023-05-02 22:20:42 +0630

    Watch user account

--- src/views/Chartroom.vue
+++ src/views/Chartroom.vue
@@ -4,9 +4,20 @@
     </div>
 </template>
 <script>
+import { useRouter } from 'vue-router';
+import getUser from '../composables/getUser';
 import Navbar from '../components/Navbar.vue'
+import { watch } from 'vue';
 export default {
   components: { Navbar },
-    
+    setup() {
+      let {user} = getUser();
+      let router = useRouter();
+      watch(user,()=>{
+        if(!user.value) {
+          router.push('/');
+        }
+      })
+    }
 }
 </script>
