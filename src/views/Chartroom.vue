53c0e1fb612199db7f2cc2b7ccfbffbc5462823d
HtetKo510217 <htetko510217@gmail.com>
2023-05-02 22:55:44 +0630

    Create new chart form

--- src/views/Chartroom.vue
+++ src/views/Chartroom.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="container">
         <Navbar />
+        <NewChatForm />
     </div>
 </template>
 <script>
+import NewChatForm from '../components/NewChatForm.vue'
 import { useRouter } from 'vue-router';
 import getUser from '../composables/getUser';
 import Navbar from '../components/Navbar.vue'
 import { watch } from 'vue';
 export default {
-  components: { Navbar },
+  components: {
+    NewChatForm, Navbar },
     setup() {
       let {user} = getUser();
       let router = useRouter();
