5bbab6b90d845052175314d87d149162012d5fcf
HtetKo510217 <htetko510217@gmail.com>
2023-05-04 23:51:26 +0630

    change our date format with date-fns library

--- src/components/chatWindow.vue
+++ src/components/chatWindow.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="chat-window">
-        <div class="messages" v-for="chartData in messages" :key="chartData.id">
+        <div class="messages" v-for="chartData in formattedMessages" :key="chartData.id">
             <div class="single">
-                <span class="created-at">{{ chartData.created_at.toDate() }}</span>
+                <span class="created-at">{{ chartData.created_at }}</span>
                 <span class="name">{{chartData.name}}</span>
                 <span class="message">{{ chartData.message }}</span>
             </div>
@@ -11,23 +11,30 @@
 </template>
 
 <script>
-import {ref} from "vue"
-import { db } from '../firebase/config';
+import { computed, ref } from 'vue'
+import { db } from '../firebase/config'
+import {formatDistanceToNow} from "date-fns"
 export default {
-    setup() {
-        let messages = ref([]);
-        db.collection('messages').orderBy('created_at').onSnapshot((snap)=>{
-            let results = [];
-            snap.docs.forEach((doc)=> {
-                let document = {...doc.data(),id:doc.id}
-               doc.data().created_at && results.push(document)
-            })
-            messages.value = results
-            console.log(messages.value);
+    setup(){
+      let messages=ref([]);
+      let formattedMessages=computed(()=>{
+        return messages.value.map((msg)=>{
+            let formatTime=formatDistanceToNow(msg.created_at.toDate())
+            return {...msg,created_at:formatTime}
+        })// [{}]
+      })
+      db.collection("messages").orderBy("created_at").onSnapshot((snap)=>{
+        let results=[];//this onsnapshot run again and array empty again
+        snap.docs.forEach((doc)=>{
+          let document={...doc.data(),id:doc.id}
+          doc.data().created_at && results.push(document);
         })
-        return {messages}
+          messages.value=results;
+      })
+      return {messages,formattedMessages};
     }
 }
+
 </script>
 
 <style>
