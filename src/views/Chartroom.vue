f3ee6339b8ede75a216c75225bdbf6044085b3f3
HtetKo510217 <htetko510217@gmail.com>
2023-05-04 22:37:00 +0630

    create chatWindow component and add design

--- src/views/Chartroom.vue
+++ src/views/Chartroom.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container">
         <Navbar />
+        <ChatWindow />
         <NewChatForm />
     </div>
 </template>
 <script>
+import ChatWindow from '../components/chatWindow.vue'
 import NewChatForm from '../components/NewChatForm.vue'
 import { useRouter } from 'vue-router';
 import getUser from '../composables/getUser';
@@ -12,6 +14,7 @@ import Navbar from '../components/Navbar.vue'
 import { watch } from 'vue';
 export default {
   components: {
+    ChatWindow,
     NewChatForm, Navbar },
     setup() {
       let {user} = getUser();
